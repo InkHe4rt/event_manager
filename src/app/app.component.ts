0529e03b6bd3579367bdaeb94fafe8bbe4c4a407
InkHe4rt <mischi.dubler@gmail.com>
2019-06-12 10:17:24 +0200

    removed unused template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,6 @@ import {Component, Inject} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // template: `<div>
-  //
-  //   <ul>
-  //     <li *ngFor="let message of mail.messages">{{message.text}}</li>
-  //   </ul>
-  //
-  //     <app-simple-form
-  //      *ngFor="let message of mail.messages"
-  //      [message]="message.text"
-  //      (update)="onUpdate(message.id, $event.text)"
-  //     >
-  //
-  //     </app-simple-form>
-  //   <script src="vendor/jquery/jquery.min.js"></script>
-  //   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  // </div>`,
-  // styles:[`
-  // app-simple-form{
-  //   margin-bottom: 10px ;
-  // }
-  // `]
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
